0b5da43f86943197233a09a7c6f18a2c5151a45a
amatt13 <anders.matthiassen@gmail.com>
2023-02-07 20:41:42 +0100

    Moved folders around

--- BossFightSource/Startup.cs
+++ BossFightSource/Startup.cs
@@ -1,12 +1,8 @@
-using BossFight.Controllers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using System;
-using System.Net.WebSockets;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace BossFight
 {
