b0f58cafd12e5ede9fed464978540c43088e1822
amatt13 <anders.matthiassen@gmail.com>
2023-03-28 21:48:32 +0200

    #35 added test framework - Added an actual test class and an example one

---
+++ BossFight.Tests/ExperienceCalculatorTests.cs
@@ -0,0 +1,23 @@
+namespace BossFight.Tests;
+
+public class ExperienceCalculatorTests
+{
+    [Theory]
+    [InlineData(100, 20, 10, 5)]  // levelsBelow >= 10
+    [InlineData(100, 20, 1, 5)]  // levelsBelow >= 10
+    [InlineData(100, 20, 11, 24)]  // levelsBelow == 9
+    [InlineData(100, 20, 12, 43)]  // levelsBelow == 8
+    [InlineData(100, 20, 13, 62)]  // levelsBelow == 7
+    [InlineData(100, 20, 14, 81)]  // levelsBelow == 6
+    [InlineData(100, 1, 1, 100)]
+    [InlineData(100, 1, 2, 100)]
+    [InlineData(100, 1, 3, 100)]
+    [InlineData(100, 1, 4, 25)]  // pXP * (playerLevel_decimal / monsterLevel_decimal)
+    [InlineData(100, 1, 10, 10)]  // pXP * (playerLevel_decimal / monsterLevel_decimal)
+    [InlineData(100, 1, 8, 13)]  // Math.Ceiling(result)
+    public void CalcXpPenalty(int pXP, int pPlayerLevel, int? pMonsterLevel, int pExpected)
+    {
+        var result = ExperienceCalculator.CalcXpPenalty(pXP, pPlayerLevel, pMonsterLevel);
+        Assert.Equal(pExpected, result);
+    }
+}
---
+++ BossFight.Tests/TestExample.cs
@@ -0,0 +1,10 @@
+namespace BossFight.Tests;
+
+public class TestExample
+{
+    [Fact]
+    public void Test1()
+    {
+        Assert.True(1 == 1);
+    }
+}
