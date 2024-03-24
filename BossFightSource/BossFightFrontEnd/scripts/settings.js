5345817b37be4c8c32cfb149aae4d3bedaa10e77
Anders Lykke Matthiassen <anders.matthiassen@gmail.com>
2024-03-24 15:18:31 +0100

    #36 new settings dialog

---
+++ BossFightSource/BossFightFrontEnd/scripts/settings.js
@@ -0,0 +1,12 @@
+const playerSettingsButton = document.getElementById("playerSettings");
+const playerSettingsCloseDialogButton = document.getAnimations("playerSettingsDialogCloseButton");
+
+playerSettingsButton.addEventListener("click", function onOpen(){
+    // show settings dialog
+    let dialog = document.getElementById("playerSettingsDialog");
+    dialog.style.display = "block";
+});
+
+function closeSettingsDialog() {
+    pl
+}
